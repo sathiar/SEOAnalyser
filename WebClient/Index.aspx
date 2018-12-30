064b7a902e2f7ee83c862f12b6c86d9539bc4c12
Sathia <rsathia88@gmail.com>
2018-12-31 01:57:57 +0800

    Seo Analyser Solution

---
+++ WebClient/Index.aspx
@@ -0,0 +1,78 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="WebClient.Index" ValidateRequest="false" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Table runat="server" style="width: 100%;">
+            <asp:TableHeaderRow Width="100%">
+                <asp:TableCell Font-Size="XX-Large" Width="100%" BackColor="LightBlue">Welcome to SEO analyser</asp:TableCell>
+            </asp:TableHeaderRow>
+        </asp:Table><br /><br />
+
+        <div class="row">
+            <div class="col-md-6">
+                <h2>Text Analyzer</h2>
+                <p>User submits a text in English, page filters out stop-words (e.g. ‘or’, ‘and’, ‘a’, ‘the’ etc), calculates number of occurrences on the page of each word, number of occurrences on the page of each word listed in meta tags, number of external links in the text.</p>
+                <asp:LinkButton ID="btnTextAnalyser" runat="server" Text="Try Text Analyser" OnClick="TextAnalyser_Click"/>
+            </div>
+            <div class="col-md-6">
+                <h2>URL Analyzer</h2>
+                <p>User submits a URL, page filters out stop-words (e.g. ‘or’, ‘and’, ‘a’, ‘the’ etc), calculates number of occurrences on the page of each word, number of occurrences on the page of each word listed in meta tags, number of external links in the text.</p>
+                <asp:LinkButton ID="btnUrlAnalyser" runat="server" Text="Try URL Analyser" OnClick="UrlAnalyser_Click" />
+            </div><br /> <br /> 
+
+            <asp:Label ID="labelErrorText" runat="server"></asp:Label>
+            <asp:TextBox ID="txtWords" runat="server" TextMode="MultiLine" Rows="1" Width="100%" BorderColor=""></asp:TextBox>
+        </div><br />
+
+        <asp:Button ID="buttonAnalyze" runat="server" Text="Analyze" OnClick="Analyze_Click" />
+        <asp:Button ID="toggleEditStopWords" runat="server" Text="View Stop Words" OnClick="toggleEdit_Click" /><br />
+
+        <asp:Table ID="tblStopWords" runat="server" style="width: 100%;">
+            <asp:TableHeaderRow>
+                <asp:TableCell Font-Size="X-Large">Stop Words</asp:TableCell>
+            </asp:TableHeaderRow>
+            <asp:TableRow>
+                <asp:TableCell>
+                    <asp:TextBox ID="textStopWords" runat="server" TextMode="MultiLine" Width="100%" Rows="3"></asp:TextBox>
+                </asp:TableCell>
+            </asp:TableRow>
+        </asp:Table>     
+        <br /><br />
+
+        <asp:Label ID="labelExternalLinkNumber" runat="server"></asp:Label><br /><br />
+        <table>
+            <tr>
+                <td valign="top">
+                    <asp:GridView ID="gridViewWords" AllowSorting="true" runat="server" OnSorting="gridViewWords_Sorting" AutoGenerateColumns="False" Caption="Words">
+                        <Columns>
+                            <asp:BoundField DataField="Key" HeaderText="Word" SortExpression="Key" />
+                            <asp:BoundField DataField="Value" HeaderText="Frequency" SortExpression="Value" />
+                        </Columns>
+                    </asp:GridView>
+                </td>
+                <td valign="top">
+                    <asp:GridView ID="gridViewKeywords" AllowSorting="true" runat="server" OnSorting="gridViewKeywords_Sorting" AutoGenerateColumns="False" Caption="Keywords">
+                        <Columns>
+                            <asp:BoundField DataField="Key" HeaderText="Word" SortExpression="Key" />
+                            <asp:BoundField DataField="Value" HeaderText="Frequency" SortExpression="Value" />
+                        </Columns>
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+<%--<script type="text/javascript">
+var t = document.getElementById('textUrlOrText');
+if (document.getElementById('radioAnalyzeUrl').checked)
+ t.rows = 1;
+else
+t.rows = 20;
+</script>--%>
+</html>
---
+++ WebClient/Index.aspx.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Web.UI.WebControls;
+
+namespace WebClient
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        bool isUrlPassed;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                labelErrorText.Text = "";
+                txtWords.BorderColor = Color.Empty;
+                using (StreamReader stream = new StreamReader(Path.Combine(Server.MapPath("~"),
+                        @"util\WordsToIgnore.txt")))
+                {
+                    textStopWords.Text = stream.ReadToEnd();
+                }
+                tblStopWords.Visible = false;
+            }
+        }
+
+        private void createDataGrid(GridView v, Dictionary<string, int> dic, string gridName)
+        {
+            if (dic == null)
+            {
+                v.DataSource = null;
+                v.DataBind();
+                return;
+            }
+
+            var dt = new DataTable();
+            DataColumn column;
+            column = new DataColumn
+            {
+                DataType = Type.GetType("System.String"),
+                ColumnName = "Key",
+                ReadOnly = true,
+                Unique = false
+            };
+            dt.Columns.Add(column);
+
+            column = new DataColumn
+            {
+                DataType = Type.GetType("System.Int32"),
+                ColumnName = "Value",
+                ReadOnly = true,
+                Unique = false
+            };
+            dt.Columns.Add(column);
+
+            foreach (var word in dic)
+            {
+                DataRow dr = dt.NewRow();
+                dr["Key"] = word.Key;
+                dr["Value"] = word.Value;
+                dt.Rows.Add(dr);
+            }
+
+            Session[gridName] = dt;
+            v.DataSource = Session[gridName];
+            v.DataBind();
+        }
+
+        private delegate Dictionary<string, int> GetDicDelegate();
+
+        private void analyseWords(GridView resultsGrid, GetDicDelegate del_GetDictionary, string gridName)
+        {
+            createDataGrid(resultsGrid, del_GetDictionary(), gridName);
+        }
+
+        protected void toggleEdit_Click(object sender, EventArgs e)
+        {
+            tblStopWords.Visible = !tblStopWords.Visible;
+        }
+
+        protected void Analyze_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                isUrlPassed = (txtWords.Rows == 1) ? true : false;
+                var counter = new util.Analyser(txtWords.Text, isUrlPassed, textStopWords.Text);
+                analyseWords(gridViewWords, counter.GetWordsDictionary, "wordsTable");
+                analyseWords(gridViewKeywords, counter.GetKeywordsDictionary, "keywordsTable");
+
+                labelExternalLinkNumber.Text = String.Format("Number of external links: {0}", counter.GetExternalLinksNumber());
+            }
+            catch (UriFormatException)
+            {
+                labelErrorText.Text = "Incompatible url format";
+                txtWords.BorderColor = Color.Red;
+            }
+            catch (WebException ex)
+            {
+                labelErrorText.Text = ex.Message;
+                txtWords.BorderColor = Color.Red;
+            }
+        }
+
+        protected void TextAnalyser_Click(object sender, EventArgs e)
+        {
+            txtWords.Text = "Input your text here to analyse text";
+            txtWords.Rows = 20;
+            isUrlPassed = false;
+        }
+
+        protected void UrlAnalyser_Click(object sender, EventArgs e)
+        {
+            txtWords.Text = "http://www.google.com";
+            txtWords.Rows = 1;
+            isUrlPassed = true;
+        }
+        private string getSortDirection(string column)
+        {
+            string sortDirection = "ASC";
+            string sortExpression = ViewState["SortExpression"] as string;
+
+            if (sortExpression != null)
+            {
+                if (sortExpression == column)
+                {
+                    string lastDirection = ViewState["SortDirection"] as string;
+                    if ((lastDirection != null) && (lastDirection == "ASC"))
+                    {
+                        sortDirection = "DESC";
+                    }
+                }
+            }
+
+            ViewState["SortDirection"] = sortDirection;
+            ViewState["SortExpression"] = column;
+
+            return sortDirection;
+        }
+
+        private void sortTable(string tableName, string sortExpression, GridView gv)
+        {
+            var dt = Session[tableName] as DataTable;
+            if (dt != null)
+            {
+                dt.DefaultView.Sort = sortExpression + " " + getSortDirection(sortExpression);
+                gv.DataSource = Session[tableName];
+                gv.DataBind();
+            }
+        }
+
+        protected void gridViewWords_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            sortTable("wordsTable", e.SortExpression, gridViewWords);
+        }
+
+        protected void gridViewKeywords_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            sortTable("keywordsTable", e.SortExpression, gridViewKeywords);
+        }
+
+    }
+}
